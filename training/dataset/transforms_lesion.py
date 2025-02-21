19d3cd07cd4002964fe1e78a073ae51db4fdfa5e
Sam Church <sdchurch@wisc.edu>
2025-02-21 16:37:18 -0600

    Adding new transforms file

---
+++ training/dataset/transforms_lesion.py
@@ -0,0 +1,27 @@
+from torchvision.transforms.v2 import GaussianBlur, GaussianNoise
+import matplotlib.pyplot as plt
+class RandomGaussianNoise:
+    def __init__(self, mean=0, sigma=0.1, clip=True):
+        self.noise = GaussianNoise(mean, sigma, clip)
+
+    def __call__(self, datapoint, **kwargs):
+        for i in range(len(datapoint.frames)):
+            datapoint.frames[i].data = self.noise(datapoint.frames[i].data)
+
+        return datapoint
+
+class RandomGaussianBlur:
+    def __init__(self, kernel_size=3, sigma=(0.0, 2.0)):
+        self.blur = GaussianBlur(kernel_size, sigma)
+
+    def __call__(self, datapoint, **kwargs):
+        for i in range(len(datapoint.frames)):
+            plt.subplot(1, 2, 1)
+            plt.show(datapoint.frames[i])
+            datapoint.frames[i].data = self.blur(datapoint.frames[i].data)
+            plt.subplot(1, 2, 2)
+            plt.show(datapoint.frames[i])
+            plt.savefig('example.png')
+            assert 1 == 2
+
+        return datapoint
